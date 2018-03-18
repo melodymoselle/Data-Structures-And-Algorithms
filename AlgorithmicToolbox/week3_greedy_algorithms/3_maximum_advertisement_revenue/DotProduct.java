351769729a432a4bdcc9dd32b2325460f6b65ab8
Melmo <melodymoselle@gmail.com>
2018-03-17 18:19:52 -0700

    Week 3, Problem 3, Dot Product

--- AlgorithmicToolbox/week3_greedy_algorithms/3_maximum_advertisement_revenue/DotProduct.java
+++ AlgorithmicToolbox/week3_greedy_algorithms/3_maximum_advertisement_revenue/DotProduct.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class DotProduct {
     private static long maxDotProduct(int[] a, int[] b) {
-        //write your code here
         long result = 0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+
         for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
+          result += (long)a[i] * (long)b[i];
         }
         return result;
     }
