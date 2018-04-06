c74b8257afcad93c05ceff2a40e1d9deda3d182d
Melmo <melodymoselle@gmail.com>
2018-04-05 22:17:16 -0700

    Week 5, problem 1, Change money with dynamic programming

--- AlgorithmicToolbox/week5_dynamic_programming1/1_money_change_again/ChangeDP.java
+++ AlgorithmicToolbox/week5_dynamic_programming1/1_money_change_again/ChangeDP.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class ChangeDP {
     private static int getChange(int m) {
-        //write your code here
-        return m / 4;
+        int[] coins = {1, 3, 4};
+        double[] minCoins = new double[m+1];
+        minCoins[0] = 0;
+        for (int i = 1; i <= m; i++) {
+          minCoins[i] = Double.POSITIVE_INFINITY;
+          for (int c : coins) {
+            if (i >= c) {
+              double numCoins = minCoins[i - c] + 1;
+              if (numCoins < minCoins[i]) {
+                minCoins[i] = numCoins;
+              }
+            }
+          }
+        }
+        return (int)minCoins[m];
     }
 
     public static void main(String[] args) {
