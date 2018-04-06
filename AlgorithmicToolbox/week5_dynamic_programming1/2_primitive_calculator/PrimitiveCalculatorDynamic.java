dde230e08c1ef86d240a5709fe5befb7b2b4b3a0
Melmo <melodymoselle@gmail.com>
2018-04-06 00:36:54 -0700

    primitive calculator attempt 2

--- AlgorithmicToolbox/week5_dynamic_programming1/2_primitive_calculator/PrimitiveCalculatorDynamic.java
+++ AlgorithmicToolbox/week5_dynamic_programming1/2_primitive_calculator/PrimitiveCalculatorDynamic.java
@@ -7,17 +7,27 @@ public class PrimitiveCalculatorDynamic {
         sequence.add(new ArrayList<Integer>(Arrays.asList(1)));
         sequence.add(new ArrayList<Integer>(Arrays.asList(1,2)));
         sequence.add(new ArrayList<Integer>(Arrays.asList(1,3)));
+        int[] count = new int[n+1];
         for (int i = 4; i <= n; i++) {
           List<Integer>  temp = new ArrayList<Integer>();
           temp.add(i);
           if (i%3 == 0) {
-            temp.addAll(0, sequence.get(i/3));
+            if (count[i-1] < count[i/3]) {
+              temp.addAll(0, sequence.get(i-1));
+            } else {
+              temp.addAll(0, sequence.get(i/3));
+            }
           } else if (i%2 == 0) {
-            temp.addAll(0, sequence.get(i/2));
+            if (count[i-1] < count[i/2]) {
+              temp.addAll(0, sequence.get(i-1));
+            } else {
+              temp.addAll(0, sequence.get(i/2));
+            }
           } else {
             temp.addAll(0, sequence.get(i-1));
           }
           sequence.add(temp);
+          count[i] = temp.size();
         }
         return sequence.get(n);
     }
